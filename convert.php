22a295a0815bc318423000bcb427ae7d420a3372
NazeefaMu <103749878+NazeefaMu@users.noreply.github.com>
2022-04-18 23:10:00 +0530

    Add files via upload

---
+++ convert.php
@@ -0,0 +1,34 @@
+<?php
+
+//DB connection
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$db = "uoc_system";
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+    //Merge files based on date
+    $sql = 'select substring_index(substring_index(filename,"_", 1), "_", -1) as date_result,COUNT(*) as number_of_files,GROUP_CONCAT(content) as content from file_data group by date_result;';
+    $retval =mysqli_query($conn,$sql) or die($conn->error);
+
+    if(! $retval ) {
+        die('Could not get data: ' . mysqli_error());
+    }
+    echo "Fetched data successfully\n";
+    foreach($retval as $row){
+        //replace multiple line breaks with one
+        $tab_removed_content=preg_replace("/[\r\n]+/", "\n", $row['content']);
+        //replace ,## breaks with ##
+        $comma_removed_content=preg_replace("/,##/", "##", $tab_removed_content);
+        //replace gibberish
+        $gibberish_removed_content=preg_replace("/'<U+200B>'/", "", $comma_removed_content);
+        $cleaned_content=preg_replace("/\?/", "", $gibberish_removed_content);
+        $mergedFile=$row['date_result'];
+        //add cleaned content to files ordered by date
+        file_put_contents('uploaded_files/'.$mergedFile.'.txt', print_r($cleaned_content, true));
+
+}
+
+mysqli_close($conn);
+
+?>
